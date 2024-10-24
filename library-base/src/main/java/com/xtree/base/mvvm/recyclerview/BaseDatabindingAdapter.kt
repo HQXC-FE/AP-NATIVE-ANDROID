e8cbcf8fe14cddd2d9ec6d1eb1d13acabd0b1cff
hqxc-arason <hqxc.arason@gmail.com>
2024-10-24 18:52:03 +0800

    公共 databing适配器优化支持匿名内部类的itemtype使用

--- library-base/src/main/java/com/xtree/base/mvvm/recyclerview/BaseDatabindingAdapter.kt
+++ library-base/src/main/java/com/xtree/base/mvvm/recyclerview/BaseDatabindingAdapter.kt
@@ -1,5 +1,6 @@
 package com.xtree.base.mvvm.recyclerview
 
+import android.util.NoSuchPropertyException
 import android.view.View
 import com.drake.brv.BindingAdapter
 import kotlin.reflect.full.createType
@@ -13,7 +14,7 @@ class BaseDatabindingAdapter : BindingAdapter() {
     /**
      * 数据回调
      */
-    interface onBindListener{
+    interface onBindListener {
         /**
          * 可操作视图和adapter,最好不用
          */
@@ -29,16 +30,21 @@ class BaseDatabindingAdapter : BindingAdapter() {
      * 数据和视图绑定
      */
     fun initData(
-        datas:List<BindModel>,
+        datas: List<BindModel>,
         layouts: List<Int>
     ) {
 
         datas
             .distinctBy { it::class } //强制规定数据类型
             .forEach { model ->
-            val kType = model.javaClass.kotlin.createType()
-            typePool[kType] = { layouts[model.itemType] }
-        }
+                val kType = if (model.javaClass.kotlin.toString().contains("\$")) {//匿名内部类
+                    (model.javaClass.superclass as Class).kotlin.createType()
+                } else {
+                    model.javaClass.kotlin.createType()
+                }
+
+                typePool[kType] = { layouts[model.itemType] }
+            }
 
         datas.filterNot {
 
@@ -51,6 +57,25 @@ class BaseDatabindingAdapter : BindingAdapter() {
             }
         }.let { models = it }
 
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val model = getModel<Any>(position)
+        // 获取模型的 Java 类
+        val modelClass = model::class.java
+
+        // 检查是否为匿名内部类
+        if (modelClass.name.contains("\$")) {
+            // 处理匿名内部类的逻辑
+            return typePool.firstNotNullOfOrNull {
+                it.value
+            }?.invoke(model, position)
+                ?: interfacePool.firstNotNullOfOrNull {
+                    it.value
+                }?.invoke(model, position)
+                ?: throw NoSuchPropertyException("Please add item model type : addType<${model.javaClass.name}>(R.layout.item)")
+        }
+        return super.getItemViewType(position)
     }
 
 }
