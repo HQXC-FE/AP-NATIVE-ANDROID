a6aaf5ef971ca0908a4e2d61e1566ec9d34d2e04
hqxc-daniel <hqxc.daniel@gmail.com>
2025-01-01 16:29:40 +0800

    公共 优化用户体验，getToken移植，取消在启动页请求getToken接口，改为在首页请求

--- module-main/src/main/java/com/xtree/main/ui/viewmodel/SplashViewModel.java
+++ module-main/src/main/java/com/xtree/main/ui/viewmodel/SplashViewModel.java
@@ -22,7 +22,6 @@ import me.xtree.mvvmhabit.utils.SPUtils;
  * Created by marquis on 2023/11/24.
  */
 public class SplashViewModel extends BaseViewModel<MainRepository> {
-    public MutableLiveData<Void> inMainData = new MutableLiveData<>();
     public MutableLiveData<Void> reNewViewModel = new MutableLiveData<>();
     public MutableLiveData<Void> noWebData = new MutableLiveData<>();
 
@@ -30,72 +29,5 @@ public class SplashViewModel extends BaseViewModel<MainRepository> {
         super(application, model);
     }
 
-    public void getFBGameTokenApi() {
-        Disposable disposable = (Disposable) model.getApiService().getFBGameTokenApi()
-                .compose(RxUtils.schedulersTransformer())
-                .compose(RxUtils.exceptionTransformer())
-                .subscribeWith(new HttpCallBack<FBService>() {
-                    @Override
-                    public void onResult(FBService fbService) {
-                        CfLog.i("FBService****** ");
-                        SPUtils.getInstance().put(SPKeyGlobal.FB_TOKEN, fbService.getToken());
-                        SPUtils.getInstance().put(SPKeyGlobal.FB_API_SERVICE_URL, fbService.getForward().getApiServerAddress());
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        CfLog.e(t.toString());
-                    }
-                });
-        addSubscribe(disposable);
-    }
-
-    public void getFBXCGameTokenApi() {
-        Disposable disposable = (Disposable) model.getApiService().getFBXCGameTokenApi()
-                .compose(RxUtils.schedulersTransformer())
-                .compose(RxUtils.exceptionTransformer())
-                .subscribeWith(new HttpCallBack<FBService>() {
-                    @Override
-                    public void onResult(FBService fbService) {
-                        SPUtils.getInstance().put(SPKeyGlobal.FBXC_TOKEN, fbService.getToken());
-                        SPUtils.getInstance().put(SPKeyGlobal.FBXC_API_SERVICE_URL, fbService.getForward().getApiServerAddress());
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        CfLog.e(t.toString());
-                    }
-                });
-        addSubscribe(disposable);
-    }
-
-    public void getPMGameTokenApi() {
-        Disposable disposable = (Disposable) model.getApiService().getPMGameTokenApi()
-                .compose(RxUtils.schedulersTransformer())
-                .compose(RxUtils.exceptionTransformer())
-                .subscribeWith(new HttpCallBack<PMService>() {
-                    @Override
-                    public void onResult(PMService pmService) {
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_TOKEN, pmService.getToken());
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_API_SERVICE_URL, pmService.getApiDomain());
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_IMG_SERVICE_URL, pmService.getImgDomain());
-                        SPUtils.getInstance().put(SPKeyGlobal.PM_USER_ID, pmService.getUserId());
-                        inMainData.setValue(null);
-                    }
-
-                    @Override
-                    public void onError(Throwable t) {
-                        CfLog.e(t.toString());
-                        inMainData.setValue(null);
-                    }
-
-                    @Override
-                    public void onFail(BusinessException t) {
-                        super.onFail(t);
-                        inMainData.setValue(null);
-                    }
-                });
-        addSubscribe(disposable);
-    }
 
 }
