3d77441623859a0d0278a2c6a1f79ee87d345d36
hqxc-seka <hqxc.seka@gmail.com>
2025-01-05 10:28:19 +0800

    【安卓】修复主页VIP等级显示错误[HQAP2-5339]

--- module-mine/src/main/java/com/xtree/mine/data/source/http/service/HttpApiService.java
+++ module-mine/src/main/java/com/xtree/mine/data/source/http/service/HttpApiService.java
@@ -54,6 +54,7 @@ import com.xtree.mine.vo.UserUsdtConfirmVo;
 import com.xtree.mine.vo.UserUsdtTypeVo;
 import com.xtree.mine.vo.VerificationCodeVo;
 import com.xtree.mine.vo.VerifyVo;
+import com.xtree.mine.vo.VipInfoVo;
 import com.xtree.mine.vo.VipUpgradeInfoVo;
 import com.xtree.mine.vo.VirtualCashVo;
 import com.xtree.mine.vo.VirtualConfirmVo;
@@ -674,4 +675,9 @@ public interface HttpApiService {
     @PUT("/api/activity/reward/{key}")
     @Headers({"Content-Type: application/vnd.sc-api.v1.json"})
     Flowable<BaseResponse3> getOffer(@Path("key") String key, @Body Map<String, String> map);
+    /**
+     * 获取 VIP信息
+     */
+    @GET("/api/account/vipinfo")
+    Flowable<BaseResponse<VipInfoVo>> getVipInfo();
 }
