f9fb65c2e6180effa31649be9ee75579e121983f
hqxc-arason <hqxc.arason@gmail.com>
2025-01-06 15:06:29 +0800

    公共 防重点击扩展添加

--- library-base/src/main/java/com/xtree/base/mvvm/Ex.kt
+++ library-base/src/main/java/com/xtree/base/mvvm/Ex.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.text.TextWatcher
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
@@ -205,4 +206,21 @@ fun String?.plusDomainOrNot(domain: String): String {
         target = domain + separator + target
     }
     return target
+}
+
+/**
+ * 防止重复点击事件 默认0.5秒内不可重复点击
+ * @param interval 时间间隔 默认0.5秒
+ * @param action 执行方法
+ */
+var lastClickTime = 0L
+fun View.clickNoRepeat(interval: Long = 500, action: (view: View) -> Unit) {
+    setOnClickListener {
+        val currentTime = System.currentTimeMillis()
+        if (lastClickTime != 0L && (currentTime - lastClickTime < interval)) {
+            return@setOnClickListener
+        }
+        lastClickTime = currentTime
+        action(it)
+    }
 }
