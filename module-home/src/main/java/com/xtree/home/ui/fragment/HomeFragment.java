72c00e1622c4f22e6d414db2cca9ac4d9ba43ff4
hqxc-daniel <hqxc.daniel@gmail.com>
2025-02-07 15:53:27 +0800

    三方游戏 安卓APP无法显示完整的PP电子游戏

--- module-home/src/main/java/com/xtree/home/ui/fragment/HomeFragment.java
+++ module-home/src/main/java/com/xtree/home/ui/fragment/HomeFragment.java
@@ -52,7 +52,6 @@ import com.xtree.home.vo.GameVo;
 import com.xtree.home.vo.NoticeVo;
 import com.youth.banner.adapter.BannerImageAdapter;
 import com.youth.banner.holder.BannerImageHolder;
-import com.youth.banner.indicator.CircleIndicator;
 import com.youth.banner.listener.OnBannerListener;
 
 import org.greenrobot.eventbus.EventBus;
@@ -183,7 +182,6 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
             KLog.d("************");
             viewModel.getNotices(); // 获取公告
             //viewModel.getProfile(); // 获取个人信息
-            viewModel.getVipInfo(); // 获取VIP信息
             //viewModel.getFBGameTokenApi();
             //viewModel.getPMGameTokenApi();
         });
@@ -236,15 +234,6 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
             }
 
         });
-        viewModel.liveDataVipInfo.observe(getViewLifecycleOwner(), vo -> {
-            CfLog.d("*** " + vo.toString());
-            //意昂2去除 VIP选项
-           /* if (vo.sp.equals("1")) {
-                binding.tvwVip.setText("VIP " + vo.display_level); // display_level
-            } else {
-                binding.tvwVip.setText("VIP " + vo.level); // level
-            }*/
-        });
         //App更新
         viewModel.liveDataUpdate.observe(getViewLifecycleOwner(), vo -> {
             updateVo = vo;
@@ -272,19 +261,10 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
                         //热更
                     }
                 }
-
             }
 
         });
 
-        viewModel.liveDataRedPocket.observe(getViewLifecycleOwner(), vo -> {
-            CfLog.e("Check has money : " + vo.money);
-            //if (vo.status == 0) {
-            //    binding.tvwMember.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.hm_ic_member_has_red, 0, 0);
-            //} else {
-            //    binding.tvwMember.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.hm_ic_member, 0, 0);
-            //}
-        });
     }
 
     public void initView() {
@@ -304,7 +284,6 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
             selectUpdate = true;
             viewModel.getProfile(); // 获取个人信息（刷新用户余额）
         });
-        binding.bnrTop.setIndicator(new CircleIndicator(getContext())); // 增加小圆点
         //binding.bnrTop.setBannerGalleryEffect(20, 12, 0.8f);// 画廊效果
         //binding.bnrTop.setBannerRound2(20);
         binding.bnrTop.setAdapter(new BannerImageAdapter<BannersVo>(new ArrayList<>()) {
@@ -672,10 +651,6 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
         }
     }
 
-    private void checkRedPocket() {
-        viewModel.getRedPocket();
-    }
-
     /**
      * 显示更新
      *
@@ -686,8 +661,6 @@ public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeViewMode
         if (updateView != null && updateView.isShow()) {
             return;
         }
-        CfLog.e("showUpdate ----> " + vo.download_url);
-
         AppUpdateDialog dialog = new AppUpdateDialog(getContext(), isWeakUpdate, vo, new AppUpdateDialog.IAppUpdateCallBack() {
             @Override
             public void onUpdateCancel() {
