4aca0b5f7e5aff4b451413cedd127a28e7ac8fce
hqxc-seka <hqxc.seka@gmail.com>
2025-04-09 14:44:39 +0800

    赞助 新增CNYT下载页面

--- module-home/src/main/java/com/xtree/home/ui/fragment/AdsFragment.java
+++ module-home/src/main/java/com/xtree/home/ui/fragment/AdsFragment.java
@@ -1,49 +1,39 @@
 package com.xtree.home.ui.fragment;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.graphics.Bitmap;
-import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.xtree.base.global.Constant;
-import com.xtree.base.global.SPKeyGlobal;
 import com.xtree.base.router.RouterFragmentPath;
-import com.xtree.base.utils.CfLog;
-import com.xtree.base.utils.DomainUtil;
+import com.xtree.base.utils.AppUtil;
 import com.xtree.home.BR;
 import com.xtree.home.R;
 import com.xtree.home.databinding.FragmentAdsBinding;
 import com.xtree.home.ui.viewmodel.HomeViewModel;
 import com.xtree.home.ui.viewmodel.factory.AppViewModelFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import me.xtree.mvvmhabit.base.BaseFragment;
-import me.xtree.mvvmhabit.utils.SPUtils;
 
 @Route(path = RouterFragmentPath.Home.AD)
 public class AdsFragment extends BaseFragment<FragmentAdsBinding, HomeViewModel> {
-    int sslErrorCount = 0;
 
+    private String downUrl = "https://www.hiwalletapp.com/download/main";
     @Override
     public void initView() {
-        initWebView();
+        binding.adsAndroidStore.setOnClickListener(v -> {
+            openWeb();
+        });
+        binding.adsiOSStore.setOnClickListener(v -> {
+            openWeb();
+        });
     }
-
+    private void  openWeb(){
+        AppUtil.goBrowser(getContext() , downUrl);
+    }
     @Override
     protected void initImmersionBar() {
     }
@@ -68,61 +58,6 @@ public class AdsFragment extends BaseFragment<FragmentAdsBinding, HomeViewModel>
         }
     }
 
-    public void initWebView() {
-        String cookie = "auth=" + SPUtils.getInstance().getString(SPKeyGlobal.USER_TOKEN)
-                + ";" + SPUtils.getInstance().getString(SPKeyGlobal.USER_SHARE_COOKIE_NAME)
-                + "=" + SPUtils.getInstance().getString(SPKeyGlobal.USER_SHARE_SESSID)
-                + ";";
-
-        String auth = "bearer " + SPUtils.getInstance().getString(SPKeyGlobal.USER_TOKEN);
-
-        CfLog.d("Cookie: " + cookie);
-        CfLog.d("Authorization: " + auth);
-
-        Map<String, String> header = new HashMap<>();
-        header.put("Cookie", cookie);
-        header.put("Authorization", auth);
-        header.put("App-RNID", "87jumkljo"); //
-
-        binding.wvAdsMain.setFitsSystemWindows(true);
-        setWebView(binding.wvAdsMain);
-
-        binding.wvAdsMain.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                CfLog.d("onPageStarted url:  " + url);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                CfLog.d("onPageFinished url: " + url);
-                hideLoading();
-            }
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                //handler.proceed();
-                CfLog.d("onPageFinished url: " + error);
-                hideLoading();
-                if (sslErrorCount < 4) {
-                    sslErrorCount++;
-                    tipSsl(view, handler);
-                } else {
-                    handler.proceed();
-                }
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                CfLog.e("errorCode: " + errorCode + ", description: " + description + ", failingUrl: " + failingUrl);
-                hideLoading();
-                Toast.makeText(getContext(), com.xtree.base.R.string.network_failed, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        binding.wvAdsMain.loadUrl(DomainUtil.getH5Domain2() + Constant.URL_APP_CENTER, header);
-    }
-
     @Override
     public int initContentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return R.layout.fragment_ads;
@@ -140,36 +75,4 @@ public class AdsFragment extends BaseFragment<FragmentAdsBinding, HomeViewModel>
         return new ViewModelProvider(this, factory).get(HomeViewModel.class);
     }
 
-    private void hideLoading() {
-        binding.ivwAdsLoading.setVisibility(View.GONE);
-        binding.ivwAdsLoading.clearAnimation();
-    }
-
-    private void tipSsl(WebView view, SslErrorHandler handler) {
-        Activity activity = (Activity) view.getContext();
-        if (activity == null || activity.isFinishing() || activity.isDestroyed()) {
-            return; // 避免 BadTokenException
-        }
-        activity.runOnUiThread(() -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-            builder.setMessage(com.xtree.base.R.string.ssl_failed_will_u_continue); // SSL认证失败，是否继续访问？
-            builder.setPositiveButton(com.xtree.base.R.string.ok, (dialog, which) -> handler.proceed()); // 接受https所有网站的证书
-
-            builder.setNegativeButton(com.xtree.base.R.string.cancel, (dialog, which) -> handler.cancel());
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
-        });
-    }
-
-    private void setWebView(WebView webView) {
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setUseWideViewPort(true);
-        settings.setLoadWithOverviewMode(true);
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);
-        settings.setLoadsImagesAutomatically(true);
-    }
 }
