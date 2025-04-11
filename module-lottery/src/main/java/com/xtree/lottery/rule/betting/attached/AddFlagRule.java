5e008dafbf502e50a6f823dc05e19235c941f70f
hqxc-mobius <hqxc.mobius@gmail.com>
2025-04-11 16:44:11 +0800

    彩票 修复AddFlagRule上Java版本问题(XSP-1235, XSP-1233)

--- module-lottery/src/main/java/com/xtree/lottery/rule/betting/attached/AddFlagRule.java
+++ module-lottery/src/main/java/com/xtree/lottery/rule/betting/attached/AddFlagRule.java
@@ -11,6 +11,7 @@ import org.jeasy.rules.api.Facts;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Rule(name = "Add Extra Flag", description = "添加额外标记")
 public class AddFlagRule {
@@ -33,7 +34,7 @@ public class AddFlagRule {
             List<String> flags = ruleSuite.stream()
                     .filter(item -> item.matches("^add-flag-.*$"))
                     .map(item -> item.split("add-flag-")[1])
-                    .toList();
+                    .collect(Collectors.toList());
 
             Map<String, List<String>> attached;
 
