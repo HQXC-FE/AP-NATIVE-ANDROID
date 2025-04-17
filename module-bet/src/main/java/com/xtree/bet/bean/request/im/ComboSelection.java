eb17bcd90b03d4c42abf18459fdd2fe203326464
hqxc-marvin <hqxc.marvin@gmail.com>
2025-04-17 17:14:53 +0800

    场馆 IM场馆添加PlaceBet投注接口的请求实体类

---
+++ module-bet/src/main/java/com/xtree/bet/bean/request/im/ComboSelection.java
@@ -0,0 +1,8 @@
+package com.xtree.bet.bean.request.im;
+
+public class ComboSelection {
+    public int ComboSelection; // 连串过关类型
+    public double StakeAmount; // 投注金额(两位小数)
+    public double BoostedOdds; // 增值后的赔率
+    public String BoosterCode; // 赔率增值编码
+}
--- module-bet/src/main/java/com/xtree/bet/bean/request/im/PlaceBetReq.java
+++ module-bet/src/main/java/com/xtree/bet/bean/request/im/PlaceBetReq.java
@@ -1,4 +1,21 @@
 package com.xtree.bet.bean.request.im;
 
+import com.xtree.bet.bean.response.im.WagerSelectionInfo;
+
+import java.util.List;
+
 public class PlaceBetReq {
+    public int WagerType; // 1=Single, 2=Combo
+    public String CustomerIP;
+    public String ServerIP;
+    public String CustomerMACAddress;
+    public String UserAgent;
+    public boolean IsComboAcceptAnyOdds;
+    public List<WagerSelectionInfo> WagerSelectionInfos;
+    public List<ComboSelection> ComboSelections;
+    public String Token;
+    public String MemberCode;
+    public int SourceWallet; // 1=主账户, 2=免费投注
+    public String TimeStamp;
+    public String LanguageCode;
 }
