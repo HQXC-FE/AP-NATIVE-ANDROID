d5b268bfbe90c27be43d8901009260312cbc3cdc
hqxc-marvin <hqxc.marvin@gmail.com>
2025-05-03 16:15:44 +0800

    场馆 移植直播代码，优化网络请求，解决几处没有处理disposable导致的内存泄漏问题

--- mvvmhabit/src/main/java/me/xtree/mvvmhabit/base/BaseViewModel.java
+++ mvvmhabit/src/main/java/me/xtree/mvvmhabit/base/BaseViewModel.java
@@ -16,10 +16,14 @@ import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.reactivex.Flowable;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.subscribers.DisposableSubscriber;
 import me.xtree.mvvmhabit.bus.event.SingleLiveData;
+import me.xtree.mvvmhabit.http.BaseResponse;
+import me.xtree.mvvmhabit.utils.RxUtils;
 
 /**
  * Created by goldze on 2017/6/15.
@@ -291,5 +295,9 @@ public class BaseViewModel<M extends BaseModel> extends AndroidViewModel impleme
         public static String CLASS = "CLASS";
         public static String CANONICAL_NAME = "CANONICAL_NAME";
         public static String BUNDLE = "BUNDLE";
+    }
+
+    protected <T> void launchFlow(Flowable<BaseResponse<T>> flowable, DisposableSubscriber<T> subscriber) {
+        RxUtils.safeSubscribe(flowable, getmCompositeDisposable(), subscriber);
     }
 }
